9c5809997c8fd3cecdbd3955ac1f239eed3da7bb
allanBekki <90175555+allanBekki@users.noreply.github.com>
2023-07-14 18:34:24 +0200

    Add files via upload

---
+++ Spammeur/Spammeur/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Spammeur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Spam = new System.Windows.Forms.Button();
+            this.Quitter = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.Font = new System.Drawing.Font("Cascadia Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Green;
+            this.textBox1.Location = new System.Drawing.Point(65, 109);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(611, 186);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Spam
+            // 
+            this.Spam.Location = new System.Drawing.Point(709, 175);
+            this.Spam.Margin = new System.Windows.Forms.Padding(4);
+            this.Spam.Name = "Spam";
+            this.Spam.Size = new System.Drawing.Size(129, 59);
+            this.Spam.TabIndex = 4;
+            this.Spam.Text = "Coller";
+            this.Spam.UseVisualStyleBackColor = true;
+            this.Spam.Click += new System.EventHandler(this.Spam_Click);
+            // 
+            // Quitter
+            // 
+            this.Quitter.Location = new System.Drawing.Point(709, 16);
+            this.Quitter.Margin = new System.Windows.Forms.Padding(4);
+            this.Quitter.Name = "Quitter";
+            this.Quitter.Size = new System.Drawing.Size(129, 59);
+            this.Quitter.TabIndex = 5;
+            this.Quitter.Text = "Quitter";
+            this.Quitter.UseVisualStyleBackColor = true;
+            this.Quitter.Click += new System.EventHandler(this.Quitter_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(709, 109);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 59);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Copier";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(709, 242);
+            this.Stop.Margin = new System.Windows.Forms.Padding(4);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(129, 55);
+            this.Stop.TabIndex = 9;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(264, 75);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(160, 23);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Cascadia Code SemiBold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(75, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Cadence (en ms):";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(845, 341);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Quitter);
+            this.Controls.Add(this.Spam);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Cascadia Code SemiLight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Spammer.exe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Spam;
+        private System.Windows.Forms.Button Quitter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Spammeur/Spammeur/Form1.cs
@@ -0,0 +1,85 @@
+﻿using Spammeur.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Spammeur
+{
+    public partial class Form1 : Form
+    {
+        //int i;
+        String text;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+        private void Spam_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Interval = ((int)numericUpDown1.Value);  //defini le temps d'intervalle via l'outil numericUpDown du disigner
+            timer1.Enabled = true;                          //lance le collage
+
+
+        }
+
+        private void Quitter_Click(object sender, EventArgs e)
+        {
+
+            Application.Exit();             //ferme l'application           
+        }
+
+        
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            text = textBox1.Text;
+            Clipboard.SetText(text);                        // permet de copier dans le press papier
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)          //boutton stop
+        {
+            timer1.Enabled = false;                                     //arrete le timer et donc par conséquent les actions produites
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)            // à chaque tick du timer, il executera le code
+        {
+            SendKeys.Send("^{v}");                    //permet de réaliser une secession de touche clavier ----- ^ correspond a la touche Ctrl
+            SendKeys.Send("~");                      //~ correspond a la touche Entrée
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
